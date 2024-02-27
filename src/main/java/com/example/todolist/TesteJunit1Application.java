be72a57efd046b18f94e8434f8c242ae2e213e12
vinicyus.rs@gmail.com <vinicyus.rs@gmail.com>
2024-02-27 13:55:46 -0300

    Adicionando novos recursos

---
+++ src/main/java/com/example/todolist/TesteJunit1Application.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJunit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJunit1Application.class, args);
+	}
+
+}
